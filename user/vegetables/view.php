b4055d0abc8b29b0435b3903eab74b27fc18e31b
Fares Sallame <faressallam75@gmail.com>
2025-10-07 11:41:10 +0300

    adding new Procfile file

--- user/vegetables/view.php
+++ user/vegetables/view.php
@@ -1,18 +1,24 @@
 <?php
 include "../../connect.php";
 
+  
+// $statement = $con->prepare("SELECT  vegetables.*, departments.* FROM vegetables, departments 
+// WHERE vegetables.departmentid = departments.departmentid") ;  
+//    $statement->execute(array()) ; 
+//    $data = $statement->fetchAll(PDO::FETCH_ASSOC) ; 
+//    $count = $statement->rowCount() ; 
+//    if($count >0){
+//     echo json_encode(array("status"=>"success", "data" =>$data)) ;
+// } else{ 
+//        echo json_encode(array("status"=>"failure")) ;
+//    }
+
+
 getAllData(
     table: "vegetables, departments",
     where: "vegetables.departmentid = departments.departmentid",
     values: array(),
     json: true
 );
-
-// getAllData(
-//     table: "veg_dep_view",
-//     where: "1 = 1",
-//     values: array(),
-//     json: true
-// );
 
 ?>
